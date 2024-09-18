18ae2a568b1e16e7f67ab0253fb97b31b3038329
Muqadsa Qudoos <muqadsaqudoos@gmail.com>
2024-09-18 19:22:28 +0500

    first commit

---
+++ even_odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    if (a%2==0) {
+        printf("%d is even number",a);
+    }
+    else{
+        printf("%d is odd number",a);
+    }
+    return 0;
+}
+
