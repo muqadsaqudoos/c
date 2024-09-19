553a3c9019eed867239f26c05fa8fa6baf0ac9a0
Muqadsa Qudoos <muqadsaqudoos@gmail.com>
2024-09-19 22:52:28 +0500

    4.c

---
+++ ch_1/4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int p,r,t;
+    printf("Enter p:");
+    scanf("%d",&p);
+
+    printf("Enter r:");
+    scanf("%d",&r);
+
+    printf("Enter t:");
+    scanf("%d",&t);
+
+    printf("the answer is : %f",(p*r*t)/100.0);
+
+    return 0;
+}
